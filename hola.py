8e847cb8171e5262eb3559b8e379d1c6e1b1471b
Julio <julio98chocano@gmail.com>
2025-10-16 17:42:35 -0600

    Cambio rapido

--- hola.py
+++ hola.py
@@ -1,1 +1,8 @@
-"print('Hola Git')" 
+print("Hola Git")
+
+nums = [1, 2, 3, 4, 5]
+cuadrados = [x**2 for x in nums]
+# cuadrados = []
+# for x in nums:
+#     cuadrados.append(x**2)
+print(f"Cuadrados: {cuadrados}")
